e7258ba1113e0e66e366aeb6a6e447d107a70ea5
Satyam <sagupta@ucsd.edu>
2020-11-25 11:01:15 +0530

    roleAssignment is working.

--- cogs/rolesAssigner.py
+++ cogs/rolesAssigner.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+
+theReactMessage = 781027866619936810
 class rolesAssigner(commands.Cog):
 
     def __init__(self, bot):
@@ -9,8 +11,23 @@ class rolesAssigner(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        print(payload.message_id)
-        # Changes here for the role reaction
+        if ( payload.message_id == theReactMessage ):
+            guild = discord.utils.find( lambda x : x.id == payload.guild_id, self.bot.guilds )
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+            if role:
+                await payload.member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if ( payload.message_id == theReactMessage ):
+            guild = discord.utils.find( lambda x : x.id == payload.guild_id, self.bot.guilds)
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
+            
+            if role:
+                member = guild.get_member(payload.user_id)
+                if member:
+                    await member.remove_roles(role)
         
 
 def setup(bot):
