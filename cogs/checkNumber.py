a3183c028eb0bd67d2e657e23ca7709e2fb54f9a
Satyam Gupta <satyamgupta19946@gmail.com>
2021-01-27 10:54:32 +0530

    Completed picking a ticket

--- cogs/checkNumber.py
+++ cogs/checkNumber.py
@@ -2,6 +2,7 @@ import numpy as np
 from discord.ext import commands
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import random
 
 import json
 gc = gspread.service_account(filename="config/credentials.json")
@@ -15,7 +16,7 @@ firstName = 'First Name'
 startingNumbers = [300000, 400000, 500000]
 sheetsName = ['Attendance', 'Artist Alley', 'Panelists & Events']
 
-def getTickets(username):
+def getAllTickets():
     generalAttendanceSheet = gc.open_by_key(data['generalAttendanceSheet']).sheet1
     generalAttendanceSheetData = generalAttendanceSheet.get_all_records()
     artistAlleySheet = gc.open_by_key(data['artistAlleySheet']).sheet1
@@ -26,8 +27,23 @@ def getTickets(username):
     for sheets, sheetNumber in zip([generalAttendanceSheetData, artistAlleySheetData, eventSheetData], range(3)):
         start = startingNumbers[sheetNumber]
         for sheetData in sheets:
-            if sheetData[userNameQuestion] == str(username):
-                #Check if ticket has number if not assign it one
+            # ticket = (First Name, DiscordTag, TicketNumber)
+            ticketsFound.append((sheetData[firstName], sheetData[userNameQuestion], start))
+            start += 1
+    return ticketsFound
+
+def getTickets(username):
+    generalAttendanceSheet = gc.open_by_key(data['generalAttendanceSheet']).sheet1
+    generalAttendanceSheetData = generalAttendanceSheet.get_all_records()
+    artistAlleySheet = gc.open_by_key(data['artistAlleySheet']).sheet1
+    artistAlleySheetData = artistAlleySheet.get_all_records()
+    eventSheet = gc.open_by_key(data['eventSheet']).sheet1
+    eventSheetData = eventSheet.get_all_records() 
+    ticketsFound = [] 
+    for sheets, sheetNumber in zip([generalAttendanceSheetData, artistAlleySheetData, eventSheetData], range(3)): 
+        start = startingNumbers[sheetNumber] 
+        for sheetData in sheets: 
+            if sheetData[userNameQuestion] == str(username): 
                 ticketsFound.append((start, sheetsName[sheetNumber]))
             start += 1
     return ticketsFound
@@ -36,6 +52,16 @@ class checkNumber(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def pickATicket(self, ctx):
+        ticketsFound = getAllTickets()
+        winner = random.randint(0, len(ticketsFound))
+        name = ticketsFound[winner][0]
+        tag = ticketsFound[winner][1]
+        ticketnumber = ticketsFound[winner][2]
+        await ctx.send("The winner is {}, Tag: {}, Ticket Number: {}".format(name, tag, ticketnumber))
+
 
     @commands.command()
     async def checkNumber(self, ctx):
